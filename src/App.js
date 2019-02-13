b46073a1b1c37bfdafbb6cb46d32ab553023e1dd
Zoey <b00408044@g.ntu.edu.tw>
2019-02-13 17:23:01 +0800

    Handled add to-do event

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddTodo';
+
 
 class App extends Component {
     state = {
@@ -16,11 +18,19 @@ class App extends Component {
             todos
         })
     }
+    addTodo = (todo) => {
+        todo.id = Math.random()
+        let todos = [...this.state.todos, todo]
+        this.setState({
+            todos
+        })
+    }
     render() {
         return (
             <div className="to-app container">
                 <h1 className="center blue-text">To-do's</h1>
                 <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+                <AddTodo addTodo={this.addTodo} />
             </div>
         )
 
