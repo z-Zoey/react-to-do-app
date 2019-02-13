53747dcd4c5999d5d6974593b6021b69da13afc6
Zoey <b00408044@g.ntu.edu.tw>
2019-02-13 16:48:47 +0800

    Initialized App.js, Todos.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        todos: [
+            { id: 1, content: 'Play Zelda' },
+            { id: 2, content: 'Watch TV' }
+        ]
+    }
+    render() {
+        return (
+            <div className="to-app container">
+                <h1 className="center blue-text">To-do's</h1>
+                <Todos todos={this.state.todos} />
+            </div>
+        )
+
+    }
 }
 
 export default App;
